8c708af40cefbd9b30ce06669dc538879d0d15d6
Bookis Smuin <vegan.bookis@gmail.com>
2011-09-28 18:43:32 -0700

    remove validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,7 @@
-class AddableValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    p value
-    if value.to_s != 'true'
-      record.errors[:base] << "task must be confirmed"
-    end
-  end
-end
-
 class Task < ActiveRecord::Base
   belongs_to :commitment
   attr_accessor :add_task
   
   validates :due_date, :uniqueness => {:scope => :commitment_id}
-  # validates :add_task, :addable => true 
   
 end
